fcdb56d34f4b28f677537401e820b4e1a89ff0a6
LovEver_Rv <yongweili.cn@gmail.com>
2020-03-06 17:34:40 +0800

    adding try-except to avoid sudden breaking

--- cmdRepost/cmdRepost.py
+++ cmdRepost/cmdRepost.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore
 import time
+import json
 
 __all__ = ['CmdReposter']
 
@@ -23,6 +24,9 @@ class CmdReposter(QtCore.QObject):
     def server_tell(self, player, text):
         self.core.write_server('/tellraw {} {}'.format(player.name, json.dumps({'text': text, 'color': 'yellow'})))
 
+    def server_warn(self, player, text):
+        self.core.write_server('/tellraw {} {}'.format(player.name, json.dumps({'text': text, 'color': 'red'})))
+
     def on_player_input(self, pair):
         self.logger.debug('CmdReposter.on_player_input called')
         player = pair[0]
@@ -34,7 +38,15 @@ class CmdReposter(QtCore.QObject):
         
         for cmd in self.cmd_available:
             if text_list[0] == self.cmd_prefix + cmd:
-                self.cmd_available[cmd](player, text_list)
+                try:
+                    self.cmd_available[cmd](player, text_list)
+                except AttributeError:
+                    self.logger.error('Fatal: AttributeError raised.')
+                    self.server_warn(player, 'cmdRepost internal error raised.')
+                except KeyError:
+                    self.logger.error('Fatal: KeyError raised.')
+                    self.server_warn(player, 'cmdRepost internal error raised.')
+                
                 break
 
     def tp_request(self, player, text_list):
