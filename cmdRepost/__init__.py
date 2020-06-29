3562733e02cf54ff1d0ec825e67a90032848a227
LovEver_Rv <yongweili.cn@gmail.com>
2020-06-29 16:01:50 +0800

    fix load func

--- cmdRepost/__init__.py
+++ cmdRepost/__init__.py
@@ -8,4 +8,4 @@ def load(logger, core):
     # Function "load" is required by mana9er-core.
     from os import path
     config_file = path.join(core.root_dir, 'cmdRepost', 'config.json')
-    CmdReposter(logger, core, config_file)
+    return CmdReposter(logger, core, config_file)
